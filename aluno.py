ec9652ac0344c8c930403d999516387710681175
ogdudu013 <dudusantana.og013@gmail.com>
2026-03-17 17:29:32 -0300

    Delete aluno.py

--- aluno.py
+++
@@ -1,9 +0,0 @@
-total_aulas = float (input("Digite o total de aulas: "))
-faltas = int(input("Digite o Número de faltas: "))
-
-frequencia = ((total_aulas - faltas) /total_aulas) * 100
-
-if frequencia >= 75:
-  print("Aprovado")
-else:
-  print("Reprovado")
