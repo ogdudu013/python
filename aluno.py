757cc00bde4a5ad08d0518ace96ef855e4544535
ogdudu013 <dudusantana.og013@gmail.com>
2026-03-17 15:18:42 -0300

    Update aluno.py

--- aluno.py
+++ aluno.py
@@ -1,1 +1,9 @@
+total_aulas = int(input("Digite o total de aulas: "))
+faltas = int(input("Digite o Número de faltas: "))
 
+frequencia = ((total_aulas - faltas) /total_aulas) * 100
+
+if frequencia >= 75:
+  print("Aprovado")
+else:
+  print("Reprovado")
